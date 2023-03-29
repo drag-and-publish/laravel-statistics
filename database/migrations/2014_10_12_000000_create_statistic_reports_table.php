dacf9d7cd6796a7f5d380e08727f1eecb617640b
Egoist <epsilonelittr@gmail.com>
2023-03-29 06:48:43 +0300

    Add statistic reports table

---
+++ database/migrations/2014_10_12_000000_create_statistic_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('statistic_reports', function (Blueprint $table) {
+            $table->id();
+
+            $table->dateTime('start_date')->comment('Start date');
+            $table->dateTime('end_date')->comment('End date');
+            $table->string('type', 100)->comment('Report type');
+            $table->string('name', 100)->comment('Report name');
+            $table->json('data')->comment('Report data');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('statistic_reports');
+    }
+};
