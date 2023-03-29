f2d5192553451bb28c34abedc8250522c5d27cad
Egoist <epsilonelittr@gmail.com>
2023-03-29 06:46:23 +0300

    Add statistic table

--- database/migrations/2014_10_12_000000_create_statistics_table.php
+++ database/migrations/2014_10_12_000000_create_statistics_table.php
@@ -14,7 +14,36 @@ return new class extends Migration
         Schema::create('statistics', function (Blueprint $table) {
             $table->id();
 
-            // ...
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->bigInteger('subject_id')->nullable()->comment('URL Id');
+            $table->ipAddress('ip')->comment('Client IP address');
+            $table->text('ua_header')->nullable(true)->default(null)->comment('User Agent Header');
+
+            $table->string('city', 100)->nullable(true)->default(null)->comment('Client city');
+            $table->string('country', 100)->nullable(true)->default(null)->comment('Client country');
+            $table->string('country_alpha2', 2)->nullable(true)->default(null)->comment('Client country alpha 2 code');
+
+            $table->string('client', 100)->nullable(true)->default(null)->comment('Client');
+            $table->string('client_type', 100)->nullable(true)->default(null)->comment('Client type');
+            $table->string('client_version', 100)->nullable(true)->default(null)->comment('Client version');
+            $table->string('client_hash', 128)->comment('Client unique hash code');
+
+            $table->string('browser_lang', 5)->nullable(true)->default(null)->comment('Browser language');
+            $table->text('referrer')->length(1000)->nullable(true)->default(null)->comment('Referrer URL');
+            $table->bigInteger('views')->default(0)->comment('View count');
+
+            $table->boolean('is_bot')->default(false)->comment('Is bot?');
+            $table->boolean('is_touchable')->default(false)->comment('Is touchable?');
+            $table->boolean('is_mobile')->default(false)->comment('Is mobile?');
+            $table->boolean('is_desktop')->default(false)->comment('Is desktop?');
+            $table->string('device_type', 100)->nullable(true)->default(null)->comment('Client device type');
+            $table->string('device_brand', 100)->nullable(true)->default(null)->comment('Client device brand');
+            $table->string('device_model', 100)->nullable(true)->default(null)->comment('Client device model');
+            $table->string('os_name', 100)->nullable(true)->default(null)->comment('Operating system name');
+            $table->string('os_version', 100)->nullable(true)->default(null)->comment('Operating system version');
+            $table->string('platform', 100)->nullable(true)->default(null)->comment('CPU platform');
 
             $table->timestamps();
         });
