9c3c2b3f96686ff1756470f35140614a0f47d1cf
Egoist <epsilonelittr@gmail.com>
2023-03-31 17:05:29 +0300

    Fix typo

--- database/migrations/2014_10_12_000000_create_statistics_table.php
+++ database/migrations/2014_10_12_000000_create_statistics_table.php
@@ -14,9 +14,6 @@ return new class extends Migration
         Schema::create('statistics', function (Blueprint $table) {
             $table->id();
 
-            $table->bigIncrements('id');
-            $table->timestamps();
-
             $table->nullableMorphs('statisticable');
 
             $table->ipAddress('ip')->comment('Client IP address');
