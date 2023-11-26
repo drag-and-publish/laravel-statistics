4631a1a93bbc228f65fd367ed799dbbb2ae1cd86
relliv <epsilonelittr@gmail.com>
2023-11-26 17:21:18 +0300

    Add statistic hit model

---
+++ src/Models/StatisticHit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LaravelReady\Statistics\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use LaravelReady\Statistics\Traits\Statisticable;
+
+class StatisticHit extends Model
+{
+    use Statisticable;
+}
