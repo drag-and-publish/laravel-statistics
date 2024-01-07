35d23982dbf3828dba085e084c45ff206525b151
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-01-07 19:13:42 +0300

    Add null check

--- src/Supports/Detector.php
+++ src/Supports/Detector.php
@@ -27,16 +27,16 @@ class Detector
                 'is_touchable' => $deviceDetector->isTouchEnabled(),
                 'is_mobile' => $deviceDetector->isMobile(),
                 'is_desktop' => $deviceDetector->isDesktop(),
-                'client' => $client['name'] ? $client['name'] : null,
-                'client_type' => $client['type'] ? $client['type'] : null,
-                'client_version' => $client['version'] ? $client['version'] : null,
+                'client' => $client && $client['name'] ? $client['name'] : null,
+                'client_type' => $client && $client['type'] ? $client['type'] : null,
+                'client_version' => $client && $client['version'] ? $client['version'] : null,
                 'device_type' => $deviceDetector->getDeviceName(),
                 'device_brand' => $deviceDetector->getBrandName(),
                 'device_model' => $deviceDetector->getModel(),
                 'ua_header' => $deviceDetector->getUserAgent(),
-                'os_name' => $os['name'] ? $os['name'] : null,
-                'os_version' => $os['version'] ? $os['version'] : null,
-                'platform' => $os['platform'] ? $os['platform'] : null
+                'os_name' => $os && $os['name'] ? $os['name'] : null,
+                'os_version' => $os && $os['version'] ? $os['version'] : null,
+                'platform' => $os && $os['platform'] ? $os['platform'] : null
             ];
 
             Cache::put($hash, $data, now()->days(1));
